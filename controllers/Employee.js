3458eeccb18f47567d7c67d6830471a8ec78c34a
ViplavBilla <156365246+ViplavBilla@users.noreply.github.com>
2024-04-05 10:16:31 -0500

    instance

--- controllers/Employee.js
+++ controllers/Employee.js
@@ -67,6 +67,19 @@ exports.employee_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+// for a specific Costume.
+exports.employee_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await employee.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
+
     
     
 
