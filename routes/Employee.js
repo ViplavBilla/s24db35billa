42f0beeeab621d63daf84e69cac2de9aed324d39
ViplavBilla <156365246+ViplavBilla@users.noreply.github.com>
2024-04-17 17:48:51 -0500

    correction

--- routes/Employee.js
+++ routes/Employee.js
@@ -12,10 +12,7 @@ router.get('/', employee_controlers.employee_view_all_Page );
 /* GET detail employee page */
 router.get('/detail', employee_controlers.employee_view_one_Page);
 
-/* GET create employee page */
-router.get('/create',secured, employee_controlers.employee_create_Page);
 
-router.get('/delete',secured, employee_controlers.employee_delete_Page);
 
 // A little function to check if we have an authorized user and continue on
 // redirect to login.
@@ -26,6 +23,10 @@ const secured = (req, res, next) => {
     req.session.returnTo = req.originalUrl;
     res.redirect("/login");
     }
+    /* GET create employee page */
+router.get('/create',secured, employee_controlers.employee_create_Page);
+
+router.get('/delete',secured, employee_controlers.employee_delete_Page);
 
 /* GET create update page */
 router.get('/update',secured, employee_controlers.employee_update_Page);
